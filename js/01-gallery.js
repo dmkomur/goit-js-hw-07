3298b69b857595e82adc7a5fdf21fa76f91b30be
dmkomur <125804681+dmkomur@users.noreply.github.com>
2023-04-18 13:29:51 +0300

    add markup ht1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const imagesToPaste = galleryItems.map(item => {
+    const { preview, original, description } = item;
+
+  return  `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+      
+}
+)
+galleryRef.insertAdjacentHTML("beforeend", imagesToPaste.join(''))
+
